f9c5e2f3cf8aab763b7fe1a03dd696589bd9caa3
Galagar Glyzel <glyzelgalagar14@gmail.com>
2026-01-20 08:41:24 -0800

    sample

---
+++ backend/user-service/user-service/src/main/java/com/example/user_service/controllers/UserController.java
@@ -0,0 +1,12 @@
+package com.example.user_service.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/users/")
+class UserController {
+
+
+
+}
